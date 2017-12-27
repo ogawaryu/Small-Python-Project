b2922cfe95bd94f4555651ede76bbd8bf3a440be
Ryuichi <ogawaryuichi@gmail.com>
2017-12-27 16:09:27 -0200

    To start the project

---
+++ run.py
@@ -0,0 +1,9 @@
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), 'app'))
+from app.routes import flaskrun
+
+if __name__ == '__main__':
+    flaskrun().run(
+        debug=True,
+        port=5000
+    )
